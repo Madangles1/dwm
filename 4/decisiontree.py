08567ad6d49ebc2701325a5c3e7c7876c002aa47
Madangles1 <36791067+Madangles1@users.noreply.github.com>
2019-04-23 20:46:42 +0530

    Add files via upload

--- 4/decisiontree.py
+++ 4/decisiontree.py
@@ -1,23 +1,53 @@
+import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
-from sklearn.model_selection import train_test_split # Import train_test_split function
-from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
-col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-# load dataset
-pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
-pima.head()
-feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
-X = pima[feature_cols] # Features
-y = pima.label # Target variable
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
-# Create Decision Tree classifer object
-clf = DecisionTreeClassifier()
-
-# Train Decision Tree Classifer
-clf = clf.fit(X_train,y_train)
-
-#Predict the response for test dataset
-y_pred = clf.predict(X_test)
-# Model Accuracy, how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+def importdata():
+ balance_data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-'+
+'databases/balance-scale/balance-scale.data',sep= ',', header = None)
+ print ("Dataset Lenght: ", len(balance_data))
+ print ("Dataset Shape: ", balance_data.shape)
+ print ("Dataset: ",balance_data.head())
+ return balance_data
+def splitdataset(balance_data):
+ X = balance_data.values[:, 1:5]
+ Y = balance_data.values[:, 0]
+ X_train, X_test, y_train, y_test = train_test_split(
+ X, Y, test_size = 0.3, random_state = 100)
+ return X, Y, X_train, X_test, y_train, y_test
+def train_using_gini(X_train, X_test, y_train):
+ clf_gini = DecisionTreeClassifier(criterion = "gini",
+ random_state = 100,max_depth=3, min_samples_leaf=5)
+ clf_gini.fit(X_train, y_train)
+ return clf_gini
+def tarin_using_entropy(X_train, X_test, y_train):
+ clf_entropy = DecisionTreeClassifier(
+ criterion = "entropy", random_state = 100,
+ max_depth = 3, min_samples_leaf = 5)
+ clf_entropy.fit(X_train, y_train)
+ return clf_entropy
+def prediction(X_test, clf_object):
+ y_pred = clf_object.predict(X_test)
+ print("Predicted values:")
+ print(y_pred)
+ return y_pred 
+def cal_accuracy(y_test, y_pred):
+ print("Confusion Matrix: ",confusion_matrix(y_test, y_pred))
+ print ("Accuracy : ",accuracy_score(y_test,y_pred)*100)
+ print("Report : ",classification_report(y_test, y_pred))
+def main():
+ data = importdata()
+ X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+ clf_gini = train_using_gini(X_train, X_test, y_train)
+ clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
+ print("Results Using Gini Index:")
+ y_pred_gini = prediction(X_test, clf_gini)
+ cal_accuracy(y_test, y_pred_gini)
+ print("Results Using Entropy:")
+ y_pred_entropy = prediction(X_test, clf_entropy)
+ cal_accuracy(y_test, y_pred_entropy)
+if __name__=="__main__":
+ main()
